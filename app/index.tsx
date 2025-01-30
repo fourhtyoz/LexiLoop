18dd9dae5bca3202ff76b992db51a99e310d0431
fourhtyoz <fourhtyoz@gmail.com>
2025-01-30 12:05:19 +0300

    fixed useNetÌnfo

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { View, Text } from "react-native";
+import { useNetInfo } from '@react-native-community/netinfo';
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 import WordOfTheDayScreen from "@/screens/WordOfTheDayScreen";
@@ -10,6 +12,18 @@ const Tab = createMaterialTopTabNavigator();
 
 
 export default function App() {
+    const netInfo = useNetInfo();
+
+    if (netInfo?.isConnected === false && netInfo?.isConnected != null) {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <Text style={{ fontSize: 18, color: "red" }}>
+                No Internet Connection
+                </Text>
+            </View>
+        )
+    }
+
     return (
         <Tab.Navigator screenOptions={{ tabBarStyle: { display: "none" }, swipeEnabled: true }}>
             <Tab.Screen name="WordSearchScreen" component={WordSearchScreen} />
