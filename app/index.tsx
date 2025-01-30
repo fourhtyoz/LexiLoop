b21fb2c236911bb4ca92b4f513edf9f1b0f82375
fourhtyoz <fourhtyoz@gmail.com>
2025-01-30 11:58:27 +0300

    removed wifi connection

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { useNetInfo } from '@react-native-community/netinfo';
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 import WordOfTheDayScreen from "@/screens/WordOfTheDayScreen";
@@ -12,18 +10,6 @@ const Tab = createMaterialTopTabNavigator();
 
 
 export default function App() {
-    const netInfo = useNetInfo();
-
-    if (!netInfo?.isConnected) {
-        return (
-            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                <Text style={{ fontSize: 18, color: "red" }}>
-                No Internet Connection
-                </Text>
-            </View>
-        )
-    }
-
     return (
         <Tab.Navigator screenOptions={{ tabBarStyle: { display: "none" }, swipeEnabled: true }}>
             <Tab.Screen name="WordSearchScreen" component={WordSearchScreen} />
